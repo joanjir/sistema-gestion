9a77008bf7b3b8511f22ae45fd2c1cdc2a4286be
joanjir <84206988+joanjir@users.noreply.github.com>
2021-08-10 08:20:26 +0200

    creando htmls en los template de misitio y modificando admin.py

--- config/misitio/admin.py
+++ config/misitio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from misitio.models import *
 # Register your models here.
+admin.site.register(Evaluacion)
+admin.site.register(Evento)
+admin.site.register(ActaReun)
