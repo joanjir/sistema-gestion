f407acb798afafeb1c4e19b98f0e7bff767f739c
joanjir <84206988+joanjir@users.noreply.github.com>
2021-08-10 21:27:41 -0400

    creando htmls y adicionando configuraciones

--- config/misitio/admin.py
+++ config/misitio/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from misitio.models import *
 # Register your models here.
 admin.site.register(Evaluacion)
-admin.site.register(Evento)
+admin.site.register(EventEstudiant)
 admin.site.register(ActaReun)
